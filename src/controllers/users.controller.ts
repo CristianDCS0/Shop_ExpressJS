e30b00aab5843e08cc33504bf49df58c012b89fc
Cristian Cobaxin <cdcs030600@gmail.com>
2025-02-20 04:09:48 -0600

    more config 5

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -55,7 +55,7 @@ export const registerUser = async (req: Request, res: Response): Promise<void> =
         res.cookie('token', token, {
             httpOnly: true,
             secure: nodeEnv,
-            sameSite: 'strict',
+            sameSite: 'lax',
             maxAge: 60 * 60 * 1000,
         });
         res.status(201).json({message: "Registro exitoso"});
@@ -105,7 +105,7 @@ export const loginUsers = async (req: Request, res: Response): Promise<void> => 
         res.cookie('token', token, {
             httpOnly: true,
             secure: nodeEnv,
-            sameSite:'strict',
+            sameSite:'lax',
             maxAge: 60 * 60 * 1000,
         });
 
